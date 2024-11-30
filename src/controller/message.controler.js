9325d8b9a3939346678f2d68034d73c670fd2168
PasanSasmika <pasansasmika198@gmail.com>
2024-11-30 06:55:37 +0530

    created a getUsersForSidebar function. by it, get the sidebar users 👨👩

--- src/controller/message.controler.js
+++ src/controller/message.controler.js
@@ -1,1 +1,16 @@
-export const getUsersForSidebar = async(req,res)=>{}
+import User from "../models/user.model.js";
+
+
+
+export const getUsersForSidebar = async(req,res)=>{
+
+    try{
+        const loggedInUserId = req.user._id;
+        const filtredUsers = await User.find({_id: { $ne: loggedInUserId }}).select("-password");
+
+        res.status(200).json(filtredUsers);
+    }catch(error){
+        console.error("Error in getUserSidebar:", error.message);
+        res.status(500).json({error: "internal server error"});
+    }
+}
