ca84ad4d81deda92ae3368dffeb42ffa026fd08d
PasanSasmika <pasansasmika198@gmail.com>
2024-12-08 18:28:28 +0530

    create routing path, pages and connect pages to routing paths 📑✨

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import NavBar from './components/NavBar'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
 
   return (
     <>
-      
+    <NavBar/>  
+
+
+
+    <Routes>
+
+      <Route path="/"  element={<HomePage/>}/>
+      <Route path="/signup"  element={<SignUpPage/>}/>
+      <Route path="/login"  element={<LoginPage/>}/>
+      <Route path="/settings"  element={<SettingsPage/>}/>
+      <Route path="/profile"  element={<ProfilePage/>}/>
+
+
+
+    </Routes>
     </>
   )
 }
